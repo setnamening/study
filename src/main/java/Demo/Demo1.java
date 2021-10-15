5a578a41d7073d5c4f72d1ddf024ab9ade388555
mjn_glb <goodMorning_glb@sgg.com>
2021-10-15 18:37:58 +0800

    master2 commit

--- src/main/java/Demo/Demo1.java
+++ src/main/java/Demo/Demo1.java
@@ -6,5 +6,6 @@ public class Demo1 {
         System.out.println("Hello git");
         System.out.println("Hello git2");
         System.out.println("Hello git3");
+        System.out.println("Hello master");
     }
 }
